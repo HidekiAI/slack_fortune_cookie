9d07b401e616ad609c4d866b4055a734ae59cf21
Hideki A. Ikeda - hidekiai@kusari <HidekiAI+kusari@CodeMonkeyNinja.com>
2020-04-06 17:58:12 -0500

    Updated run.sh to make sure we do not spam slack

--- run.sh
+++ run.sh
@@ -1,7 +1,31 @@
 #!/bin/bash
+_PID=$$
+_DATETIME=$(date +%Y%m%d:%H%M)
 set -o nounset
 source ./env.conf.sh
 set +o nounset
+
+if ! [ -e $_PIDFILE ] ; then
+	# record time first, since PID may probably no longer exist
+	echo $_DATETIME > $_PIDFILE
+	echo $_PID >> $_PIDFILE
+else
+	# already running, check pid and when was the last time ran
+	_T=$(head -n 1 $_PIDFILE)
+	_P=$(tail -n 1 $_PIDFILE)	
+	if [ $_T == $_DATETIME ] ; then
+		# in same day, hour, and minute (ignores seconds)
+		exit -1
+	fi
+	if [ $_P == $_PID ] ; then
+		# same pid, why?
+		exit -1
+	fi
+
+	# could be same day and hour, but minutes differ (at the worst case, 61 seconds past)
+	echo $_DATETIME > $_PIDFILE
+	echo $_PID >> $_PIDFILE
+fi
 
 # NOTE: 'motd.txt' is currently hard-coded to index.js source code
 #_MOTD=./motd.txt
